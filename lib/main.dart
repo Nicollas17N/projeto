bc065c2be8d3a996a4a9ac6f3b85b92aa1bd378b
Nicollas17N <nicollas.mateus@aluno.ce.gov.br>
2024-04-04 07:58:51 -0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
-dvfascvaszdvasd
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
